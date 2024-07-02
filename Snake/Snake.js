cd93687bc848845b64a5ff90186fdfd9cd50fe5b
qianjingit <qianjingit@gmail.com>
2024-07-02 23:27:12 +0800

    feat:add snake

---
+++ Snake/Snake.js
@@ -0,0 +1,16 @@
+let snake = document.querySelector('#snake');
+
+//snake body
+const scale = 20;
+const rows = 20;
+const cols = 20;
+
+function Snake() {
+   const snakeElement = document.createElement("div");
+   snakeElement.style.left = `20px`;
+   snakeElement.style.top = `20px`;
+
+}
+Snake();
+
+
